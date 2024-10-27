15bbbdd37eac8ed03b0ff395ca7ccbc3e6d20aa3
aessar <raressilviulazar@gmail.com>
2024-10-27 11:19:41 +0000

    Client & Server components

---
+++ app/components/hello.jsx
@@ -0,0 +1,11 @@
+"use client";
+
+import React from 'react'
+
+function Hello () {
+    console.log("This is a CLIENT component");
+    return (
+        <div>Hello</div>
+    );
+}
+export default Hello
